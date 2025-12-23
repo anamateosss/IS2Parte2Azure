ef5032798f8318e0977d7ba817a4a51e79189977
IgnacioGDV <ignaciogdvcontact@gmail.com>
2025-12-23 16:14:32 +0100

    settings corregido

--- project/settings.py
+++ project/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'relecloud', 'static', 'res', 'img')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'relecloudgdv@gmail.com'
